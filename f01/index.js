455c1c30e934652b9f0d79d6541a592d91178ce0
Angel <loaizadavila.angeldario@gmail.com>
2020-03-08 17:54:57 +0000

    thing moved to f01

---
+++ f01/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+var app = express();
+
+app.get("/time", (request, response) => {
+	response.send(new Date());
+})
+app.listen(8080);
+
+console.log("Server ready");
